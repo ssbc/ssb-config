6f222b9fda0e3fc34c89d432b1e2d0eb4b838db7
Dominic Tarr <dominic.tarr@gmail.com>
2019-01-19 00:35:48 +1300

    calling ssbDb.whoami() doesn't test any config, because it doesn't go through the network. also removing server part because it fails if you are running sbot in the background

--- tests/server-starts.js
+++ tests/server-starts.js
@@ -1,24 +1,17 @@
 var test = require('tape')
-var Server = require('ssb-server')
+//var Server = require('ssb-server')
 var home = require('os-homedir')()
 
 var defaultConfig = require('../')
 var Config = require('../inject')
 
+//not including default tests, because they fail if you are running ssb-server in background
 test('Default config', t => {
   t.equal(defaultConfig.path, `${home}/.ssb`, 'has default ~/.ssb folder')
   t.equal(defaultConfig.connections.incoming.net[0].port, 8008, 'e.g. has default port')
   t.equal(defaultConfig.friends.dunbar, 150, 'e.g. has default dunbar number')
 
-  var server = Server(defaultConfig)
-
-  server.whoami((err, feed) => {
-    t.false(err, 'server starts')
-    t.true(feed.id.startsWith('@'), 'starts server safely')
-    server.close(() => t.end())
-  })
-  // mix: I know whoami can be run as a sync call on the server.
-  // I've found it confusing to move between remote and local and to forget remote methods must be async
+  t.end()
 })
 
 test('Custom config', t => {
@@ -28,11 +21,9 @@ test('Custom config', t => {
   t.equal(config.connections.incoming.net[0].port, 9999, 'e.g. has default port')
   t.equal(config.friends.dunbar, 1500, 'has new default dunbar number')
 
-  var server = Server(config)
-
-  server.whoami((err, feed) => {
-    t.false(err, 'server starts')
-    t.true(feed.id.startsWith('@'), 'starts server safely')
-    server.close(() => t.end())
-  })
+  t.end()
 })
+
+
+
+
