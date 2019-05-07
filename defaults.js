6c5d000f7e07263abb30a0ffb5195ef7a092c6bd
Christian Bundy <christianbundy@fraction.io>
2019-05-07 08:01:05 -0700

    Remove non-private-ip
    We want to bind on all possible interfaces unless explicitly configured
    otherwise. This may resolve an issue where ssb-ws wasn't correctly
    binding to the `localhost` IP.
    
    See: https://github.com/ssbc/patchwork/issues/1024

--- defaults.js
+++ defaults.js
@@ -1,7 +1,6 @@
 var path = require('path')
 var home = require('os-homedir')
 var merge = require('deep-extend')
-var nonPrivate = require('non-private-ip')
 var ssbKeys = require('ssb-keys')
 var get = require('lodash.get')
 
@@ -13,10 +12,6 @@ var MIN = 60 * SEC
 
 module.exports = function setDefaults (name, config) {
   var baseDefaults = {
-    // just use an ipv4 address by default.
-    // there have been some reports of seemingly non-private
-    // ipv6 addresses being returned and not working.
-    // https://github.com/ssbc/scuttlebot/pull/102
     path: path.join(home() || 'browser', '.' + name),
     party: true,
     timeout: 0,
@@ -59,13 +54,13 @@ module.exports = function setDefaults (name, config) {
   if (!config.connections.incoming) {
     config.connections.incoming = {
       net: [{
-        host: config.host || nonPrivate.v4 || '::',
+        host: config.host || '::',
         port: config.port || defaultPorts.net,
         scope: ['device', 'local', 'public'],
         transform: 'shs'
       }],
       ws: [{
-        host: config.host || nonPrivate.v4 || '::',
+        host: config.host || '::',
         port: get(config, 'ws.port', defaultPorts.ws),
         scope: ['device', 'local', 'public'],
         transform: 'shs'
