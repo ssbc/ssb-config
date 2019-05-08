8b75f55bc1a07b405fe984a0370861ee897f1f97
Christian Bundy <christianbundy@fraction.io>
2019-05-08 09:37:26 -0700

    Automatically configure `config.connections.incoming`
    This replaces the old hardcode method with a new automatic configuration
    that iterates through `os.networkInterfaces()` and returns a valid
    configuration that takes advantage of all possible interfaces.

--- defaults.js
+++ defaults.js
@@ -1,9 +1,9 @@
 var path = require('path')
 var home = require('os-homedir')
 var merge = require('deep-extend')
-var nonPrivate = require('non-private-ip')
 var ssbKeys = require('ssb-keys')
 var get = require('lodash.get')
+const os = require('os')
 
 var fixConnections = require('./util/fix-connections')
 var defaultPorts = require('./default-ports')
@@ -13,10 +13,6 @@ var MIN = 60 * SEC
 
 module.exports = function setDefaults (name, config) {
   var baseDefaults = {
-    // just use an ipv4 address by default.
-    // there have been some reports of seemingly non-private
-    // ipv6 addresses being returned and not working.
-    // https://github.com/ssbc/scuttlebot/pull/102
     path: path.join(home() || 'browser', '.' + name),
     party: true,
     timeout: 0,
@@ -56,22 +52,97 @@ module.exports = function setDefaults (name, config) {
   }
   config = merge(baseDefaults, config || {})
 
+  // We have to deal with some legacy behavior where:
+  //
+  // - net port is defined by `config.port`
+  // - ws port is defined by `config.ws.port`
+  // - other services have no canonical port config location (TODO?)
+  const getPort = (service) => {
+    const defaultPort = defaultPorts[service]
+
+    if (service === 'net' ) {
+      return get(config, 'port', defaultPort)
+    }
+    if (service === 'ws' ) {
+      return get(config, 'ws.port', defaultPort)
+    }
+
+    return defaultPort
+  }
+
   if (!config.connections.incoming) {
-    config.connections.incoming = {
-      net: [{
-        host: config.host || nonPrivate.v4 || '::',
-        port: config.port || defaultPorts.net,
-        scope: ['device', 'local', 'public'],
-        transform: 'shs'
-      }],
-      ws: [{
-        host: config.host || nonPrivate.v4 || '::',
-        port: get(config, 'ws.port', defaultPorts.ws),
-        scope: ['device', 'local', 'public'],
-        transform: 'shs'
-      }]
+    // We only use two scopes by default:
+    //
+    // - internal: inaccessible over the network
+    // - external: accessible over the network
+    //
+    const scope = {
+      internal: ['device'],
+      external: ['device', 'local', 'public']
+    }
+
+    // If `config.host` is defined then we don't need to enumerate interfaces.
+    if (config.host) {
+      config.connections.incoming = {
+        net: [{
+          host: config.host,
+          port: getPort('net'),
+          scope: scope.external,
+          transform: 'shs'
+        }],
+        ws: [{
+          host: config.host,
+          port: getPort('ws'),
+          scope: scope.external,
+          transform: 'shs'
+        }]
+      }
+    } else {
+      // Trying to hardcode reasonable defaults here doesn't seem possible.
+      //
+      // Instead, the below code enumerates all network interfaces and adds them
+      // to `config.connections.incoming` for each service in `defaultPorts`.
+
+      // If you aren't familiar, you should at least skim these docs:
+      // https://nodejs.org/api/os.html#os_os_networkinterfaces
+      const interfaces = os.networkInterfaces()
+
+      // Game plan: we're going to enumerate the services (e.g. net and ws) and
+      // return an object that looks like this:
+      //
+      // {
+      //   net: [ interface, interface, ... ]
+      //   ws: [ interface, interface, ... ]
+      // }
+      config.connections.incoming = Object.keys(defaultPorts).map((service) => {
+        return {
+          service,
+          interfaces: Object.values(interfaces).flat().filter(item => {
+            // We want to avoid scoped IPv6 addresses since they don't seem to
+            // play nicely with the Node.js networking stack. These addresses
+            // often start with `fe80` and throw EINVAL when we try to bind to
+            // them. 
+            return item.scopeid == null || item.scopeid === 0
+          }).map(item => {
+            // This bit is simple because the ssb-config options for `incoming`
+            // can either be hardcoded or directly inferred from `interfaces`.
+            return {
+              host: item.address,
+              port: getPort(service),
+              scope: item.internal ? scope.internal : scope.external,
+              transform: 'shs'
+            }
+          })
+        }
+      }).reduce((result, obj) => {
+        // This `reduce()` step is necessary because we need to return an object
+        // rather than an array. There may be a simpler way to do this.
+        result[obj.service] = obj.interfaces;
+        return result
+      }, {})
     }
   }
+
   config = fixConnections(config)
 
   if (config.keys == null) {
